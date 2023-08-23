2fc061107e8a1da2f861e784b1be6dd72e563a55
Tomate 06 13 <69756782+Tomate0613@users.noreply.github.com>
2023-08-23 20:48:33 +0200

    Make endermans immune to potions

---
+++ src/main/java/tomate/totaldragon/mixin/EnderManMixin.java
@@ -0,0 +1,20 @@
+package tomate.totaldragon.mixin;
+
+import net.minecraft.world.entity.EntityType;
+import net.minecraft.world.entity.monster.EnderMan;
+import net.minecraft.world.entity.monster.Monster;
+import net.minecraft.world.level.Level;
+import org.spongepowered.asm.mixin.Mixin;
+
+@Mixin(EnderMan.class)
+public abstract class EnderManMixin extends Monster {
+    protected EnderManMixin(EntityType<? extends Monster> entityType, Level level) {
+        super(entityType, level);
+    }
+
+    // Make endermans immune to potions so that they don't die from the dragon breath areaeffectcloud
+    @Override
+    public boolean isAffectedByPotions() {
+        return false;
+    }
+}
