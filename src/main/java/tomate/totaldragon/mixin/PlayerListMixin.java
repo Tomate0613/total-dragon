1457c73dc128f4b7a8cad8d67789f977f57af36a
Tomate 06 13 <69756782+Tomate0613@users.noreply.github.com>
2023-08-26 23:33:13 +0200

    Forward player join and leave events to FightState

---
+++ src/main/java/tomate/totaldragon/mixin/PlayerListMixin.java
@@ -0,0 +1,29 @@
+package tomate.totaldragon.mixin;
+
+import net.minecraft.network.Connection;
+import net.minecraft.server.level.ServerPlayer;
+import net.minecraft.server.players.PlayerList;
+import org.spongepowered.asm.mixin.Final;
+import org.spongepowered.asm.mixin.Mixin;
+import org.spongepowered.asm.mixin.Shadow;
+import org.spongepowered.asm.mixin.injection.At;
+import org.spongepowered.asm.mixin.injection.Inject;
+import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
+import tomate.totaldragon.FightState;
+
+import java.util.List;
+
+@Mixin(PlayerList.class)
+public class PlayerListMixin {
+    @Shadow @Final private List<ServerPlayer> players;
+
+    @Inject(at = @At("HEAD"), method = "placeNewPlayer")
+    void onPlayerJoined(Connection connection, ServerPlayer serverPlayer, CallbackInfo ci) {
+        FightState.onPlayerJoin();
+    }
+
+    @Inject(at = @At("HEAD"), method = "remove")
+    void onPlayerLeave(ServerPlayer serverPlayer, CallbackInfo ci) {
+        FightState.onPlayerLeave();
+    }
+}
