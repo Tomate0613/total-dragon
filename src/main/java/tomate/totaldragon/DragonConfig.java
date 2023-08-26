075378712e31298559241b41986cbf2d943cfb66
Tomate 06 13 <69756782+Tomate0613@users.noreply.github.com>
2023-08-26 23:23:57 +0200

    Add phase logging feature

---
+++ src/main/java/tomate/totaldragon/DragonConfig.java
@@ -0,0 +1,6 @@
+package tomate.totaldragon;
+
+// TODO Replace this with an actual config (placeholder for now)
+public class DragonConfig {
+    public static final boolean logDragonPhasesToInGameChat = false;
+}
---
+++ src/main/java/tomate/totaldragon/mixin/EnderDragonPhaseManagerMixin.java
@@ -0,0 +1,40 @@
+package tomate.totaldragon.mixin;
+
+import net.minecraft.network.chat.Component;
+import net.minecraft.server.MinecraftServer;
+import net.minecraft.server.level.ServerPlayer;
+import net.minecraft.world.entity.boss.enderdragon.EnderDragon;
+import net.minecraft.world.entity.boss.enderdragon.phases.EnderDragonPhase;
+import net.minecraft.world.entity.boss.enderdragon.phases.EnderDragonPhaseManager;
+import org.spongepowered.asm.mixin.Final;
+import org.spongepowered.asm.mixin.Mixin;
+import org.spongepowered.asm.mixin.Shadow;
+import org.spongepowered.asm.mixin.injection.At;
+import org.spongepowered.asm.mixin.injection.Inject;
+import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
+import tomate.totaldragon.DragonConfig;
+
+@Mixin(EnderDragonPhaseManager.class)
+public class EnderDragonPhaseManagerMixin {
+    @Shadow @Final private EnderDragon dragon;
+
+    @Inject(at = @At("HEAD"), method = "setPhase")
+    void setPhase(EnderDragonPhase<?> enderDragonPhase, CallbackInfo ci) {
+        if(!DragonConfig.logDragonPhasesToInGameChat)
+            return;
+
+        MinecraftServer server = this.dragon.getServer();
+
+        if(server == null)
+            return;
+
+        Component message = Component.literal("Changed phase to " + enderDragonPhase.toString());
+
+        var players = server.getPlayerList().getPlayers();
+
+        for (ServerPlayer serverPlayer : players) {
+            if(serverPlayer.hasPermissions(2))
+                serverPlayer.sendSystemMessage(message, false);
+        }
+    }
+}
