fea97e4d97c67626fc23002595db1269d665b0d3
Tomate 06 13 <69756782+Tomate0613@users.noreply.github.com>
2023-09-20 22:43:06 +0200

    EnderMans should not be agro towards the dragon

--- src/main/java/tomate/totaldragon/mixin/EnderManMixin.java
+++ src/main/java/tomate/totaldragon/mixin/EnderManMixin.java
@@ -1,10 +1,15 @@
 package tomate.totaldragon.mixin;
 
 import net.minecraft.world.entity.EntityType;
+import net.minecraft.world.entity.LivingEntity;
+import net.minecraft.world.entity.boss.enderdragon.EnderDragon;
 import net.minecraft.world.entity.monster.EnderMan;
 import net.minecraft.world.entity.monster.Monster;
 import net.minecraft.world.level.Level;
 import org.spongepowered.asm.mixin.Mixin;
+import org.spongepowered.asm.mixin.injection.At;
+import org.spongepowered.asm.mixin.injection.Inject;
+import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
 
 @Mixin(EnderMan.class)
 public abstract class EnderManMixin extends Monster {
@@ -16,5 +21,12 @@ public abstract class EnderManMixin extends Monster {
     @Override
     public boolean isAffectedByPotions() {
         return false;
+    }
+
+    @Inject(method = "setTarget", at = @At("HEAD"), cancellable = true)
+    void setTarget(LivingEntity livingEntity, CallbackInfo ci) {
+        if(livingEntity instanceof EnderDragon) {
+            ci.cancel();
+        }
     }
 }
